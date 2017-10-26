34d8c43c1030f3c0f292bc98a11285542dec7cb5
蒲瑜琦 <mf1732106@smail.nju.edu.cn>
2017-10-26 21:53:53 +0800

    .

--- src/main/java/StreamHandler.java
+++ src/main/java/StreamHandler.java
@@ -2,7 +2,10 @@ import com.mongodb.util.JSON;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
@@ -11,13 +14,14 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.bson.Document;
 import org.codehaus.jettison.json.JSONObject;
+import java.io.Serializable;
 import scala.Tuple2;
 
 /**
  * author: Qiao Hongbo
  * time: {$time}
  **/
-public class StreamHandler {
+public class StreamHandler{
     public static final String STREAM_SERVER_HOST = "114.212.245.176";
     public static final int STREAM_SERVER_PORT = 9999;
     private JavaStreamingContext jssc;
@@ -29,6 +33,7 @@ public class StreamHandler {
 //                .set("SPARK_LOCAL_IP", "114.212.242.132")
 //                .set("SPARK_")
                 .setAppName("Team13");
+        //.setJars(new String[]{"/home/puyvqi/test/NJU-CloudComputing/target/spark-streaming-jingdong-1.0-SNAPSHOT.jar"});
         jssc = new JavaStreamingContext(conf, Durations.seconds(1));
 
 
@@ -37,10 +42,27 @@ public class StreamHandler {
 
     public void defineProcess() {
         JavaReceiverInputDStream<String> lines = jssc.socketTextStream(STREAM_SERVER_HOST, STREAM_SERVER_PORT);
-        JavaDStream<Document> docs = lines.map(Document::parse);
-        JavaPairDStream<String, Integer> commentPairs = docs.mapToPair(doc -> new Tuple2<>("" + doc.get("comment_id"), 1));
-        JavaPairDStream<String, Integer> commentCounts = commentPairs.reduceByKey((cnt1, cnt2) -> cnt1 + cnt2);
-        lines.print(10);
+        JavaDStream<Document> docs = lines.map(new Function<String, Document>() {
+            @Override
+            public Document call(String s) throws Exception {
+                return Document.parse(s);
+            }
+        });
+//        JavaPairDStream<String, Integer> commentPairs = docs.mapToPair(doc -> new Tuple2<>("" + doc.get("comment_id"), 1));
+        JavaPairDStream<String, Integer> commentPairs = docs.mapToPair(new PairFunction<Document, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(Document document) throws Exception {
+                return new Tuple2<>(document.get("comment_id")+"", 1);
+            }
+        });
+//        JavaPairDStream<String, Integer> commentCounts = commentPairs.reduceByKey((cnt1, cnt2) -> cnt1 + cnt2);
+        JavaPairDStream<String, Integer> commentCounts = commentPairs.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) throws Exception {
+                return integer+integer2;
+            }
+        });
+        commentCounts.print(10);
 
     }
 
